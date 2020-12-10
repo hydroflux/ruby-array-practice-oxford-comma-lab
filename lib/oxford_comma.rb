d117a84a5a8604038726c8ee4ef4fde89275c258
Jack Hubert <jphubert93@gmail.com>
2020-12-10 14:30:35 -0700

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array)
+    if array.length < 3
+        array.join(" and ")
+    else
+        new_array = array.join(", ")
+        new_array.insert((new_array.rindex(",") + 1)," and")
+    end
 end
